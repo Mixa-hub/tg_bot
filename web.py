19cae1b1a8b7b79be179fa70dadd0395f0e392b9
Mixa-hub <timevibex@gmail.com>
2025-06-16 12:52:16 +0300

    🚑 Fix: порт + Flask для Render Web Service

--- web.py
+++ web.py
@@ -4,9 +4,9 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Бот працює!"
+def index():
+    return "🤖 TG Bot is alive!"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
